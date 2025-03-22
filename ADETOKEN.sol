24631d1fc522d1cf88df6c461fb4f1bfbaad3931
ADE49 <earnbyads94@gmail.com>
2025-03-22 12:37:50 +0530

    Create ADETOKEN.sol

---
+++ ADETOKEN.sol
@@ -0,0 +1,6 @@
+git init  
+git add .  
+git commit -m "Initial ADE Token contract"  
+git branch -M main  
+git remote add origin https://github.com/your-username/ADE-Token.git  
+git push -u origin main
